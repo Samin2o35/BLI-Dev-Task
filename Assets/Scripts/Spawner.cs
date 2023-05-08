318b27b9560d46aa7155fb147ce31115f8062448
Samin2o35 <saminmohammed65@gmail.com>
2023-05-08 20:11:29 +0600

    Shape spawner script update
    Updated spawner script where upon left mouse click, instantiate object at mouse click point.
    
    Bug : Object spawned falls through floor

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject cubePrefab;
+    public GameObject spherePrefab;
+    [SerializeField] private bool isPlacingCube;
+    [SerializeField] private bool isPlacingSphere;
+
     void Start()
     {
-        
+        isPlacingCube = false;
+        isPlacingSphere = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        SpawnSelectedShape();
+    }
+
+    public void SpawnSelectedShape()
+    {
+        //mouse left click
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            //check if cube slected
+            if (isPlacingCube && !isPlacingSphere)
+            {
+                //ray cast for placing at mouse position
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    //spawn cube at mouse position with no rotation
+                    Instantiate(cubePrefab, hit.point, Quaternion.identity);
+                }
+            }
+
+            //check if sphere slected
+            if (isPlacingSphere && !isPlacingCube)
+            {
+                //ray cast for placing at mouse position
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                {
+                    //spawn sphere at mouse position with no rotation
+                    Instantiate(spherePrefab, hit.point, Quaternion.identity);
+                }
+            }
+        }
+    }
+    
+    public void SelectCubePrefab()
+    {
+        //indicate that we are now placing a cube
+        isPlacingCube = true;
+    }
+
+    public void SelectSpherePrefab()
+    {
+        //indicate that we are now placing a sphere
+        isPlacingSphere = true;
     }
 }
